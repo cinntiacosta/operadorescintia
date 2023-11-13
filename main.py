bd3592d7ef83b726d65322ec75dc0fab58d5e5aa
cinntiacosta <150087373+cinntiacosta@users.noreply.github.com>
2023-11-13 21:49:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,114 @@
+#Operadores#
+
+# Soma
+x = 5
+x = 5 + 2 
+print(x)
+
+y = 5
+y += 5
+print(y)
+
+# Subtração
+x = 5
+x = 5 - 3
+print(x)
+
+y = 3
+y -= 2 
+print(y)
+
+# Multiplicação
+x = 5
+x = 5 * 2 
+print(x)
+
+y = 5
+y *= 2
+print(y)
+
+# Divisão
+x = 10
+x = 10 / 2
+print(x)
+
+y = 5
+y /= 5
+print(y)
+
+# Divisão inteira - resto da divisão
+x = 5
+x = 5 % 5 
+print(x)
+
+y = 5
+y %= 5 
+print(y)
+
+# Potência
+x = 2
+x = 2 ** 3
+print(x)
+
+y = 5
+y **= 2
+print(y)
+
+# Quoeficiente inteiro da divisão
+x = 5
+x //= 5 
+print(x)  
+
+# Atribuição
+x = 5
+print(x)
+
+x = 5
+print(pow(x, 2)) 
+
+input("--------------------")
+
+numero1 = int(input("Digite um número inteiro: "))
+numero2 = int(input("Digite outro número inteiro: "))
+
+soma = numero1 + numero2
+
+print("A soma do primeiro número", numero1, "+", numero2, "é igual a", soma)
+
+# Atribuição
+x = 5
+x = x + 5
+x += 2
+
+x = x - 4
+x -= 2
+
+x = x * 5
+x *= 5
+
+x = x / 5
+x /= 5
+
+x %= 5
+x **= 5
+x //= 5
+print("x =", x)
+
+# Comparação
+
+print(4 > 1)
+
+print(5 > 3)
+
+print(5 >= 3)
+
+x = 5
+y = 3
+
+print(x < y)
+
+print(x <= y)
+
+print(x == y)
+
+print(x != y)
